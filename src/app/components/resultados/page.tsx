0f7bd78968242021bd7d29b4cdf873c86d3315e1
AndersonIvanildo <andersonivanildo@protonmail.com>
2025-02-23 17:36:59 -0300

    inserindo resultados

---
+++ src/app/components/resultados/page.tsx
@@ -0,0 +1,10 @@
+import styles from "@/app/components/resultados/resultado.module.css"
+import BarraPesquisa from "../barra_pesquisa/page"
+
+export default function Conteudo() {
+    return (
+        <div className={styles.conteudo}>
+            
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
-import BarraPesquisa from "./components/barra_pesquisa/page";
+import styles from "@/app/page.module.css"
 import Menu from "./components/menu/page";
+import BarraPesquisa from "./components/barra_pesquisa/page";
+import Conteudo from "./components/resultados/page";
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.all}>
       <Menu />
       <BarraPesquisa />
+      <Conteudo />
     </div>
   );
 }
