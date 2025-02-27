aead104ed7738c59225e937bbb094a403ec34873
AndersonIvanildo <andersonivanildo@protonmail.com>
2025-02-26 21:01:14 -0300

    inicializando atulizações do projeto

---
+++ src/app/components/cardAtividade/page.tsx
@@ -0,0 +1,9 @@
+import styles from "@/app/components/cardAtividade/atividade.module.css"
+
+export default function CardAtividade() {
+    return (
+        <div className={styles.card}>
+            ola
+        </div>
+    )
+}
--- src/app/components/resultados/page.tsx
+++ src/app/components/resultados/page.tsx
@@ -1,10 +1,11 @@
 import styles from "@/app/components/resultados/resultado.module.css"
 import BarraPesquisa from "../barra_pesquisa/page"
+import CardAtividade from "../cardAtividade/page"
 
 export default function Conteudo() {
     return (
         <div className={styles.conteudo}>
-            texto
+            <CardAtividade />
         </div>
     )
 }
